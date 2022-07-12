c3b163b079abbd5dc57ac0ec25aade33b71b7510
gabrieljgm <gabrieljgm@users.noreply.github.com>
2022-07-12 15:43:01 -0500

    Actualización carga imagenes

--- src/View/frmSuperHeroes.java
+++ src/View/frmSuperHeroes.java
@@ -10,8 +10,11 @@ import Classes.Poder;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
 import java.util.Iterator;
 import java.util.LinkedList;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JCheckBox;
 import javax.swing.JOptionPane;
@@ -23,15 +26,23 @@ import javax.swing.JOptionPane;
 public final class frmSuperHeroes extends JFrame  implements ActionListener{  
     
     
+  private javax.swing.JPanel jPanel1;
+  private javax.swing.JPanel jPanel2;
+  private javax.swing.JLabel jLabel1;
+    
     
   public frmSuperHeroes()
   {
-    super("SuperHeroes Store");
+      
+    //super("SuperHeroes Store");
     ctlSuperHeroes ctlSHeroes = new ctlSuperHeroes();
     Cliente cliente = ctlSHeroes.inicializarCliente();
     LinkedList<Poder> poder = ctlSHeroes.inicializarPoder();
-    setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    add(makeWelcomePanel(cliente), "North");
+    
+    //initComponents(cliente, poder);
+    
+   setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+   add(makeWelcomePanel(cliente));
     makeItemsPanel(poder);
 
     pack();
@@ -57,13 +68,28 @@ public final class frmSuperHeroes extends JFrame  implements ActionListener{
           final JPanel p = new JPanel();
           Poder nextPoder = iterator.next();
           JCheckBox cb=new JCheckBox(nextPoder.getNombre());  
-          //JLabel nombre = new JLabel(nextPoder.getNombre());
           JLabel valor = new JLabel("$"+Double.toString(nextPoder.getPrecio()));
           JLabel duracion = new JLabel(Long.toString(nextPoder.getDuracion())+" horas");
-          //JLabel imagen = new JLabel();
+          JLabel imagen = new JLabel();
+          imagen.setIcon(new ImageIcon(getClass().getResource(nextPoder.getImagen()))); // NOI18N
+          p.add(imagen);
           p.add(cb);
           p.add(valor);
           p.add(duracion);
+          cb.addItemListener(new ItemListener() {
+              @Override
+              public void itemStateChanged(ItemEvent e) {
+                  System.out.println(e.getStateChange());
+                  System.out.println(e.getID());
+                  //System.out.println(e.getItem());
+                  JCheckBox obj = (JCheckBox) e.getItem();
+                  System.out.println(obj.getText());
+                  
+                  System.out.println(e.getItemSelectable());
+                 
+                  //JOptionPane.showMessageDialog(null,"Item(s) adicionados");  
+              }
+          });
           this.add(p);
       }
       JButton b = new JButton("Agregar al Carrito");
@@ -76,6 +102,45 @@ public final class frmSuperHeroes extends JFrame  implements ActionListener{
   @Override
     public void actionPerformed(ActionEvent e){  
        JOptionPane.showMessageDialog(this,"Item(s) adicionados");  
+       
+    }
+
+    private void initComponents(Cliente cliente, LinkedList<Poder> poder) {
+        
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        
+        jPanel1 = new javax.swing.JPanel();
+        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
+        jLabel1 = new JLabel();
+        jLabel1.setText("Bienvenido "+cliente.getNombre() + " " + cliente.getApellido());
+        jPanel1.setLayout(jPanel1Layout);
+        jPanel1.add(jLabel1);
+        add(jPanel1);
+        
+        // inicializamos el panel de poderes
+        jPanel2 = new javax.swing.JPanel();
+        javax.swing.GroupLayout jPanel2Layout = new javax.swing.GroupLayout(jPanel2);
+        jPanel2.setLayout(jPanel2Layout);
+        Iterator<Poder> iterator=poder.iterator();
+        while(iterator.hasNext()){
+            final JPanel p = new JPanel();
+            Poder nextPoder = iterator.next();
+            JCheckBox cb=new JCheckBox(nextPoder.getNombre());  
+            JLabel valor = new JLabel("$"+Double.toString(nextPoder.getPrecio()));
+            JLabel duracion = new JLabel(Long.toString(nextPoder.getDuracion())+" horas");
+            JLabel imagen = new JLabel();
+            imagen.setIcon(new ImageIcon(getClass().getResource(nextPoder.getImagen()))); // NOI18N
+            p.add(imagen);
+            p.add(cb);
+            p.add(valor);
+            p.add(duracion);
+            jPanel2.add(p);
+        }
+        add(jPanel2);
+        pack();
+        setVisible(true);
+        
+
     }
     
 }
